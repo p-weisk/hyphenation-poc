a8c16642ac4a264e50c51e370b2d4f86f8aec5bc
Paul Weiske <weiske@punkt.de>
2018-08-21 11:30:01 +0200

    TASK: Use textContent instead of innerHTML

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 (function() {
-	var Hyphenation = require('./node_modules/hypher/lib/hypher.js'),
-    german = require('./node_modules/hyphenation.de/lib/de.js'),
-    hyphenationAgent = new Hyphenation(german);
-	[...document.getElementsByClassName('hyphenatepls')].forEach( function(current) {
-		current.innerHTML = hyphenationAgent.hyphenateText(current.innerHTML);
-	});
+	const eligibleElements = [...document.querySelectorAll('.hyphenatepls')];
+	if (eligibleElements.length > 0) {
+		var Hyphenation = require('./node_modules/hypher/lib/hypher.js'),
+			german = require('./node_modules/hyphenation.de/lib/de.js'),
+			hyphenationAgent = new Hyphenation(german);
+		eligibleElements.forEach( function(current) {
+			current.textContent = hyphenationAgent.hyphenateText(current.textContent);
+		});
+	}
 })();
